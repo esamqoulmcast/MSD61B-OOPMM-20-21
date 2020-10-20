56dda54f04b91462834ea940a4efedd8d9e950b5
daren-scerri <daren.scerri@mcast.edu.mt>
2020-10-20 13:55:09 +0200

    first commit

---
+++ L01/Assets/BallSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject myCirclePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 mypos = new Vector3(7.5f, 7.5f, 0);
+        GameObject mycircle1 = Instantiate(myCirclePrefab, mypos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TASK1/Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       transform.position = new Vector3(2,2,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}

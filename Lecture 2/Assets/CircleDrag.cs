8f17ecd4ce5d439701b06b1a6d37b8904f593256
daren-scerri <daren.scerri@mcast.edu.mt>
2020-10-27 11:46:26 +0100

    Lecture 3

---
+++ Lecture 2/Assets/CircleDrag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleDrag : MonoBehaviour
+{
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f, 0f, 10f);
+        this.gameObject.transform.position = mousePos;
+    }
+
+}
---
+++ Lecture 2/Assets/CircleSpawnerMouse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawnerMouse : MonoBehaviour
+{
+    Vector3 mousePos;
+    public GameObject myCirclePrefab;
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f, 0f, 10f);
+
+        if (Input.GetButtonDown("Fire1") && (count<5))
+        {
+            Instantiate(myCirclePrefab, mousePos, Quaternion.identity);
+            count++;
+        }
+        
+        Debug.Log(mousePos);
+
+
+    }
+}
